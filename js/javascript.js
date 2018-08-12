9fd6d70d0558ac26d90e496dc037ad46c94796fb
rnguzi <bleachhddh@gmail.com>
2018-08-12 08:46:07 +0800

    fundemental version

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+var total = document.documentElement.clientHeight;
+
+
+document.getElementById("container").style.height=total+"px";
+document.getElementById("container").style.backgroundColor="#282828";
+
+
+//content.hide();
