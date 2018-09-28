aecae6f1cf5bcd47baa18cd6904d124a91287e5d
rnguzi <bleachhddh@gmail.com>
2018-09-28 18:30:50 +0800

    main page update

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,6 @@
 var total = document.documentElement.clientHeight;
 
-
-document.getElementById("container").style.height=total+"px";
-document.getElementById("container").style.backgroundColor="#282828";
+//document.getElementById("container").style.backgroundColor="#e6e6e6";
 
 
 //content.hide();
